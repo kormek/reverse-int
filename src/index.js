5162f7aa96d74282a7c9730ec29f66f21c7cc247
Vladimir <anikol200@gmail.com>
2021-02-13 23:05:08 +0300

    fix: 20/20 tests done!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    n = n.toString();
+    console.log(n);
+    n = n.split('');
+     console.log(n);
+    n = n.reverse();
+     console.log(n);
+     let res = "";
+    for(let i = 0;i<n.length;i++){
+      res+=n[i];
+    }
+     
+    return parseInt(res);
 }
